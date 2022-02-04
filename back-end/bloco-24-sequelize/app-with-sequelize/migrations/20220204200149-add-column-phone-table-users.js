6e7096778b638f62dbc5a748383f2aa711b83ada
Monnylly <monnylly@gmail.com>
2022-02-04 18:43:21 -0300

    aprendendo sequelize

---
+++ back-end/bloco-24-sequelize/app-with-sequelize/migrations/20220204200149-add-column-phone-table-users.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'phone_num', {
+      type: Sequelize.STRING,
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    down: async (queryInterface, Sequelize) => {
+      await queryInterface.removeColumn('Users', 'phone_num');
+    }
+  }
+};
