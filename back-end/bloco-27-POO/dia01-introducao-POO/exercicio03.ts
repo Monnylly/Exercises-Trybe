3a02a82988bf0bb44976f981b8aab965d5eee21a
Monnylly <monnylly@gmail.com>
2022-03-09 10:24:17 -0300

    exercicio 3

--- back-end/bloco-27-POO/dia01-introducao-POO/exercicio03.ts
+++ back-end/bloco-27-POO/dia01-introducao-POO/exercicio03.ts
@@ -31,15 +31,18 @@ class ItemPedido {
   get nome(): string {
     return this._nome;
   }
+
   set nome(value: string) {
     if(value.length < 4) {
       throw new Error('o nome deve conter no mínimo 4 caracteres');
     }
     this._nome = value;
   }
+
   get preco(): number {
     return this._preco;
   }
+
   set preco(value: number) {
     if(value < 0) throw new Error('o preco deve ser positivo');
     this._preco = value;
@@ -49,8 +52,53 @@ class ItemPedido {
 
 
 class Pedido{
-  private _nome: string = String();
+  private _cliente: Cliente;
   private _itens: ItemPedido[] = [];
   private _formaPagto: string;
   private _desconto: number = 0;
+
+  constructor(cliente: Cliente, itens: ItemPedido[], formaPagto: string, desconto: number) {
+    this._cliente = cliente;
+    this._itens = itens;
+    this._formaPagto = formaPagto;
+    this._desconto = desconto;
+  }
+
+  get cliente(): Cliente {
+    return this._cliente;
+  }
+
+  set cliente(value: Cliente) {
+    this._cliente = value;
+  }
+
+  get itens(): ItemPedido [] {
+    return this._itens;
+  }
+
+  set itens(value: ItemPedido[]) {
+    if(value.length === 0) {
+      throw new Error('o pedido deve ter pelo menos um item.');
+    }
+  }
+
+  get formaPagto(): string {
+    return this._formaPagto;
+  }
+
+  set formaPagto(value: string) {
+    this._formaPagto = value;
+  }
+
+  get desconto(): number {
+    return this._desconto;
+  }
+
+  set desconto(value: number) {
+    if(value < 0) {
+      throw new Error('o desconto não pode ser valor negativo');
+    }
+    this._desconto = value;
+  }
+
 }
