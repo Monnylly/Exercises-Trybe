5358505f52e55728c10ec3f15a208162b9d523e2
Monnylly <monnylly@gmail.com>
2022-05-30 19:14:03 -0300

    fix: criando arquivos e fazendo exercicios

---
+++ cienciasDaComputação/bloco-33-introduçãoPython/dia-1-aprendendo/exercicio.py
@@ -0,0 +1,70 @@
+# Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
+
+
+def maior(n1, n2):
+    if n2 > n1:
+        return n2
+    return n1
+
+
+# Exercício 2: Calcule a média aritmética dos valores contidos em uma lista.
+def media(num):
+    total = 0
+    for number in num:
+        total += number
+    return total / len(num)
+
+
+#  Python sabe multiplicar sequências! Por exemplo, 3 * 'bla'
+# resulta em blablabla. Isso se aplica a listas também, caso você precise.
+
+
+# Exercício 3: Faça um programa que, dado um valor n qualquer,
+# tal que n > 1, imprima na tela um quadrado feito de asteriscos
+# de lado de tamanho n. Por exemplo:
+
+
+def asteriscos(n):
+    for row in range(n):
+        print(n * "*")
+
+
+# Crie uma função que receba uma lista de
+# nomes e retorne o nome com a maior quantidade de caracteres
+# . Por exemplo, para
+# ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]o
+# retorno deve ser "Fernanda".
+# 🦜 Uma dica: Utilize a função len() para verificar o tamanho do nome.
+
+
+def maiorNome(names):
+    big_name = names[0]
+    for name in names:
+        if len(name) > len(big_name):
+            big_name = name
+    return big_name
+
+
+def type_of_triangle(side1, side2, side3):
+    is_triangle = (
+        side1 + side2 > side3
+        and side2 + side3 > side1
+        and side1 + side3 > side2
+    )
+    if not is_triangle:
+        return "não é triângulo"
+    elif side1 == side2 == side3:
+        return "equilátero"
+    elif side1 == side2 or side2 == side3 or side1 == side3:
+        return "isósceles"
+    else:
+        return "escaleno"
+
+
+def triang_ast(n):
+    for row in range(1, n + 1):
+        print(row * "*")
+
+
+if __name__ == "__main__":
+    print(maior(7, 9))
