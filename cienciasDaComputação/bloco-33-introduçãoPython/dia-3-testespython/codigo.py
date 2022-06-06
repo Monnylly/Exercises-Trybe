25b0e14ea724bc95a8f8072d3be5ce845df2f61c
Monnylly <monnylly@gmail.com>
2022-06-06 10:09:54 -0300

    exercicios test python

---
+++ cienciasDaComputação/bloco-33-introduçãoPython/dia-3-testespython/codigo.py
@@ -0,0 +1,8 @@
+def is_odd(number):
+    "returns true if a number is odd, otherwise return false"
+    return number % 2 != 0
+
+
+def division(a_number, other_number):
+    "Return the division of a_number by other_number"
+    return a_number / other_number
